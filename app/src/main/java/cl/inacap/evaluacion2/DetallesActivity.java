5004c560ca88837b1a942f2ad03920786e8deb81
DarkGiioOo <giovanni.barra02@inacapmail.cl>
2022-11-07 19:30:14 -0300

    evaluacion2

---
+++ app/src/main/java/cl/inacap/evaluacion2/DetallesActivity.java
@@ -0,0 +1,68 @@
+package cl.inacap.evaluacion2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cl.inacap.evaluacion2.modelo.ComprasDatabaseHelper;
+import cl.inacap.evaluacion2.modelo.Producto;
+
+public class DetallesActivity extends AppCompatActivity {
+    public Producto producto;
+    public Intent intent;
+    public ComprasDatabaseHelper helper=new ComprasDatabaseHelper(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+        //Obtener el nombre del producto
+        intent=getIntent();
+        String nombreProducto=(String) intent.getExtras().get("nombreProducto");
+
+        //Traer el producto desde la base de datos
+        producto= helper.getProducto(nombreProducto);
+
+
+        //Nombre del producto
+        TextView txtNombre=(TextView) findViewById(R.id.txtNombre);
+        txtNombre.setText(producto.getNombre());
+
+        //Cantidad y unidad
+        TextView txtCantidad=(TextView) findViewById(R.id.txtCantidad);
+        txtCantidad.setText("Cantidad: "+producto.getCantidad()+" "+producto.getUnidad());
+
+
+        //Estado y Botón
+        TextView txtEstado=(TextView) findViewById(R.id.txtEstado);
+        Button estado=(Button) findViewById(R.id.estado);
+
+        if(producto.isEstado()==Producto.COMPRADO)
+        {
+            txtEstado.setText("Comprado");
+            estado.setText("Marcar como Pendiente");
+        }
+        else
+        {
+            txtEstado.setText("Pendiente");
+            estado.setText("Marcar como comprado");
+        }
+        estado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                producto.setEstado(!producto.isEstado());
+                //Actualizar en la base de datos
+                String msg=helper.cambiarEstado(producto);
+                Toast.makeText(DetallesActivity.this,msg,Toast.LENGTH_SHORT).show();
+
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/cl/inacap/evaluacion2/ListaProductosActivity.java
@@ -0,0 +1,67 @@
+package cl.inacap.evaluacion2;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import cl.inacap.evaluacion2.modelo.ComprasDatabaseHelper;
+import cl.inacap.evaluacion2.modelo.Producto;
+
+public class ListaProductosActivity extends ListActivity {
+    ListView lista;
+    ComprasDatabaseHelper helper=new ComprasDatabaseHelper(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        cargarLista();
+    }
+
+    public void cargarLista()
+    {
+        lista=getListView();
+        ArrayList<Producto> productos= helper.listaProductos(); //Cargamos la lista desde la base de datos
+        ArrayAdapter<Producto> listaAdapter=new ArrayAdapter<Producto>(
+                this, android.R.layout.simple_list_item_1, productos
+        );
+        lista.setAdapter(listaAdapter);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Leer el elemento de la lista que se seleccionó
+                Object obj=lista.getItemAtPosition(i);
+                //Transformamos la línea a String
+                String linea=obj.toString();
+                //Obtenemos el nombre separando el String en un arreglo de tipo String
+                String[] separar=linea.split(":");
+                //Llamar a DetallesActivity
+                Intent intent=new Intent(ListaProductosActivity.this,DetallesActivity.class);
+                //Enviamos el nombre del producto
+                intent.putExtra("nombreProducto",separar[0]);
+                startActivityForResult(intent,1);
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==1)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                cargarLista();
+            }
+        }
+    }
+}
